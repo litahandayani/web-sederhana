fbe38e68ff0a42702aad6a30b4faccdbf7658c9b
litahandayani <lita.tampubolon15@gmail.com>
2020-05-11 18:12:18 +0700

    Menambakan Navbar

--- bootstrap/js/style.js
+++ bootstrap/js/style.js
@@ -1,1 +1,10 @@
-$(function () { objectFitImages() });
+// event pada saat di klik
+$('.page-scroll').on('click', function (e) {
+    //ambil isi href
+    var href= $(this).attr('href');
+    //tangkap elemen ybs
+    var elemenHref=$(href);;
+    //pindahkan scroll
+    $('body').animate({ scrollTop:elemenHref.offset.top -50
+    }, 1000, 'swing');
+})
